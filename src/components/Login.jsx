d894364fc6e3342cd8a4c8d4e140b03282eb0802
Abolfazl <iabolfazl83@yahoo.com>
2025-02-15 23:57:32 +0330

    project initialization

---
+++ src/components/Login.jsx
@@ -0,0 +1,45 @@
+import {useRef, useState} from "react";
+
+export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
+  const email = useRef()
+  const password = useRef()
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const emailValue = email.current.value;
+    const emailIsValid = emailValue.trim() !== '' || emailValue.includes('@');
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+    console.log('Sending HTTP request');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" autoComplete="username" ref={email}/>
+          <div className="control-error">{emailIsInvalid && `please enter a valid email address`}</div>
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" autoComplete="current-password" ref={password}/>
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button type="reset" className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
